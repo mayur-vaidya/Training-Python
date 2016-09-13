fa839457183b9fa2bc559a4e7bfb679fd0df0870
mayurvaidya09 <mayur@newput.com>
2016-09-13 16:30:05 +0530

    String Methods

---
+++ MyStrings2.py
@@ -0,0 +1,11 @@
+class MyString2:
+	name="MAYUR"
+	small="mayur"
+	
+	print name == small
+	print name != small
+	print name >= small
+	print name <= small
+	print name < small
+	print name > small
+	print name <> small
