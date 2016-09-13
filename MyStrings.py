ad0b6f36cb5589ed479ee7fac611271f2bf3640f
mayurvaidya09 <mayur@newput.com>
2016-09-13 16:29:16 +0530

    Program on String.

---
+++ MyStrings.py
@@ -0,0 +1,9 @@
+name='Mayur'
+length = len(name) #This method is used for finding the length of the string and returns int value
+j=0
+for i in range (-1, -length-1, -1):
+	print name[i],"\t",name[j],"\t"
+	j+=1
+for a in range (0, length):
+	print name[a],
+
